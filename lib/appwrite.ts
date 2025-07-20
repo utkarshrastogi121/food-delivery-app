f223e8c2f5785158b6154f540e68384320d59d6b
Utkarsh Rastogi <utkarsh.2023ug1077@iiitranchi.ac.in>
2025-07-20 15:38:12 +0530

    appwrite setup

---
+++ lib/appwrite.ts
@@ -0,0 +1,6 @@
+export const appwriteConfig={
+    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+    platform: "com.utk.fooddelivery",
+    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID
+}
+
